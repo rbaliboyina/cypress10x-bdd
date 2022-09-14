ecc56a91bf89c44a4af6a59603d03a8cb5b1dc15
rbaliboyina <raja.sekhar.cmr@gmail.com>
2022-09-14 10:34:21 +0200

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+module.exports = defineConfig({
+  projectId: 'u35dv9',
+  e2e: {
+    async setupNodeEvents(on, config) {
+      // implement node event listeners here
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
+    },
+    specPattern: "cypress/e2e/features/*.feature",
+    chromeWebSecurity: false
+  },
+});
